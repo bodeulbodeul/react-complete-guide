d38d608b280a91265663a5bcb15fe9034b8f828e
bodeul <ysymfw@gmail.com>
2023-07-18 09:15:43 +0900

    9강 Portal 적용

--- practice-user-add/src/components/UI/Modal.jsx
+++ practice-user-add/src/components/UI/Modal.jsx
@@ -1,21 +1,30 @@
+import ReactDom from "react-dom";
 import Button from "./Button";
 import Card from "./Card";
 import styles from "./Modal.module.css";
 
 export default function Modal(props) {
+  const Backdrop = (props) => <div className={styles.backdrop}></div>;
+
+  const Overlay = (props) => (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>{props.message}</div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onClose}>Okay</Button>
+      </footer>
+    </Card>
+  );
+
   return (
-    <div>
-      <div className={styles.backdrop}>
-        <Card className={styles.modal}>
-          <header className={styles.header}>
-            <h2>{props.title}</h2>
-          </header>
-          <div className={styles.content}>{props.message}</div>
-          <footer className={styles.actions}>
-            <Button onClick={props.onClose}>Okay</Button>
-          </footer>
-        </Card>
-      </div>
-    </div>
+    <>
+      {ReactDom.createPortal(<Backdrop />, document.getElementById("backdrop-root"))}
+      {ReactDom.createPortal(
+        <Overlay title={props.title} message={props.message} onClose={props.onClose} />,
+        document.getElementById("overlay-root")
+      )}
+    </>
   );
 }
