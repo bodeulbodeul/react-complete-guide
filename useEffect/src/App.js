071ee7efce6113ab7252511f5962974f2421d5ec
bodeul <ysymfw@gmail.com>
2023-07-18 09:20:09 +0900

    10강 useEffect를 활용하여 사용자 입력 시 유효성 검사

---
+++ useEffect/src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+
+function App() {
+  // useEffect를 활용해 localStorage에 로그인 여부 확인
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const isLoggedInInStorage = localStorage.getItem("isLoggedIn");
+    isLoggedInInStorage && setIsLoggedIn(isLoggedInInStorage);
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
