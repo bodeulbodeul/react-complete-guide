e545dbeb97134ec4bed6b5e9d40333fc048b26b0
bodeul <ysymfw@gmail.com>
2023-07-22 15:06:35 +0900

    11강 useEffect를 활용하여 장바구니에 상품 추가 시  이펙트 추가

--- food-order-app/src/components/Layout/HeaderCartButton.jsx
+++ food-order-app/src/components/Layout/HeaderCartButton.jsx
@@ -1,14 +1,27 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 export default function HeaderCartButton(props) {
+  const [isBump, setIsBump] = useState(false);
+
   const cartContext = useContext(CartContext);
 
-  const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+  const { items } = cartContext;
 
-  const btnClasses = `${classes.button} ${classes.bump}`;
+  const totalAmount = items.reduce((prevCount, item) => (prevCount += item.amount), 0);
+
+  const btnClasses = `${classes.button} ${isBump && classes.bump}`;
+
+  useEffect(() => {
+    items.length > 0 && setIsBump(true);
+    const timer = setTimeout(() => setIsBump(false), 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
     <button className={btnClasses} onClick={props.onOpen}>
       <span className={classes.icon}>
